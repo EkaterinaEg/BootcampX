4d311563069b03111dad883f08ba789da864078b
Ekaterina Egorova <ekaterinan.egorova@gmail.com>
2023-11-15 15:51:07 -0500

    change queries to avoid SQL injection

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -6,20 +6,19 @@ const pool = new Pool({
   host: "localhost",
   database: "bootcampx",
 });
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+const values = [`%${cohortName}%`, limit];
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
 
-const [cohort_name, limit] = process.argv.slice(2);
-// WHERE cohorts.name LIKE '%${process.argv[2]}%'
-// LIMIT ${process.argv[3] || 5};
 pool
-  .query(
-    `
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${cohort_name}%'
-LIMIT ${limit};
-`
-  )
+  .query(queryString, values)
   .then((res) => {
     res.rows.forEach((user) => {
       console.log(
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,19 +7,19 @@ const pool = new Pool({
   database: "bootcampx",
 });
 
-const cohort_name = process.argv[2];
-pool
-  .query(
-    `
-    SELECT DISTINCT teachers.name AS name, cohorts.name AS cohort
+const cohortName = process.argv[2];
+const value = [`%${cohortName}%`];
+const queryString = `
+SELECT DISTINCT teachers.name AS name, cohorts.name AS cohort
     FROM teachers
     JOIN assistance_requests ON teachers.id = assistance_requests.teacher_id
     JOIN students ON students.id = assistance_requests.student_id
     JOIN cohorts ON cohorts.id = students.cohort_id
-    WHERE cohorts.name LIKE '%${cohort_name}%'
+    WHERE cohorts.name LIKE $1
     GROUP BY teachers.name, cohorts.name; 
-`
-  )
+`;
+pool
+  .query(queryString, value)
   .then((res) => {
     res.rows.forEach((teacher) => {
       console.log(`${teacher.cohort}: ${teacher.name}`);
